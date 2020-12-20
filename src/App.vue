9d0824f605a390f75113033147a3e973cb4fe860
wozijimo <346302975@qq.com>
2020-12-20 08:41:47 +0800

    1213点名作业

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,116 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+<div class="cls-all">
+      
+      <div class="cls-top">
+          <!-- 左侧按钮 -->
+          <div class="cls-button">
+              <div id="id_inputend" v-if="inputName == ``">{{prompt}}</div>
+              <input id="id_inpu t" type="text" v-model="inputName"/>
+              <button v-on:click="btinput()">录入</button>
+              <button v-on:click="randomstar()" >随机点名开始</button>
+              <button v-on:click="randomend()">随机点名结束</button>
+              
+          </div>
+          <!-- 右侧展示 -->
+          <div id="id_show" class="cls-show">{{showright}}</div>
+          
+      </div>
+
+      <!-- 展示数据库 -->
+      <div id="id_showarray" class="cls-showarray">{{namearr}}</div>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data(){
+      return{
+        namearr:[],
+        //录入框内容
+        inputName:"",
+        //右侧展示内容
+        showright:"",
+        //随机点名
+        si:null,
+        prompt:"请输入姓名",
+      }
+    },
+
+    methods: {
+
+      // 录入按钮
+      btinput(){
+        console.log(this.inputName)
+        if(this.inputName!= ""){
+          this.namearr.push(this.inputName);
+          this.inputName= "";
+        }
+      },
+      // 点名开始按钮
+      randomstar(){
+        if(this.namearr != undefined){
+            this.si = setInterval(()=>{
+              let len = this.namearr.length;
+              let randomnumber = Math.random()*len;
+              // // 生成随机数向下取整
+              let randomInt = Math.floor(randomnumber);
+              console.log(randomInt);
+              this.showright = this.namearr[randomInt];
+          },300);
+        }else{
+
+        }
+        
+      },
+      //停止按钮
+      randomend(){
+        clearInterval(this.si);
+      }
+    }
   }
-}
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .cls-all{
+      width: 600px;
+      height: auto;
+      background-color: #eeeeee;
+  }
+
+  .cls-top{
+      display: flex;
+      flex-flow: row;
+      justify-content: center;
+      align-items: center;
+  }
+
+  .cls-button{
+      height: 200px;
+      display: flex;
+      flex-flow: column;
+      justify-content: space-around;
+      align-items: center;
+  }
+
+  .cls-show{
+      width: 300px;
+      height: 300px;
+      background-color: #ffffff;
+      border-radius: 150px;
+      line-height: 300px;
+      text-align: center;
+  }
+
+  .cls-showarray{
+      width: 600px;
+      height: 300px;
+      background-color: #dddddd;
+      /* overflow: hidden; */
+      word-wrap: wrap;
+  }
 </style>
